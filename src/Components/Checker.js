4a32b0ae844ab51e2d22e53ea36cc7bf4362ce47
profnike <profnike@gmail.com>
2021-09-03 20:12:41 +0100

    corrections

--- src/Components/Checker.js
+++ src/Components/Checker.js
@@ -9,12 +9,17 @@ import axios from 'axios'
 import Form from 'react-bootstrap/Form'
 import React, {useEffect,useState} from 'react';
 import {Link} from "react-router-dom"
+import Spinner from 'react-bootstrap/Spinner'
+import NumberFormat from 'react-number-format'
 
 function Checker() {
   const [latest,setLatest]=useState([])
   const [value,setValue]=useState([])
   const [country, setCountry]=useState("")
   const [countryname, setCountryname]=useState("")
+  const [loading, setLoading]=useState(false)
+  const [post, setPost]=useState(null)
+  const [content, setContent]=useState({display:"none"})
  
   const [headInputs, setHeadInputs]=useState({fontWeight:"bold",
 display:"none", fontSize:"medium",})
@@ -30,20 +35,37 @@ const [searchDatar, setSearchDatar]=useState("")
     
   }
   useEffect(()=>{
+    
     axios
+     
     .all([
       axios.get("https://corona.lmao.ninja/v3/covid-19/all"),
       axios.get("https://corona.lmao.ninja/v3/covid-19/countries")
     ])
+   
 
     .then(res =>{
       setLatest(res[0].data);
       setValue(res[1].data)
+      const {body}=res[0]
+      setPost(body)
+     setLoading(true)
+   
     })
-    .catch(err =>{
+     
+   
+    
+  
+ .catch (err =>{
+    
       console.log(err)
-    }) 
+    })
+    
+   
+    
   },[]);
+ var NumberFormat= require('react-number-format')
+  
   const date= new Date(parseInt(latest.updated))
   const latestUpdated=date.toString();
 
@@ -54,22 +76,27 @@ const [searchDatar, setSearchDatar]=useState("")
   
  countryFilter.map((data, index) =>{
     return (
-      <Col key={index}>
+      <Col
+      className="colum" key={index} style={{display:"flex",justifyContent:"center",
+     
+       }}>
+     
       <Card
       key={index}
       bg="light"
       text="dark"
       className="text-center"
-      style={{margin:"5px", }}>
-        <Card.Img variant="top" src={data.countryInfo.flag} style={{height:"140px"}} />
+      id="carrd"
+      style={{margin:"5px",lineHeight:"10px", }}>
+        <Card.Img variant="top" src={data.countryInfo.flag} style={{height:"140px",}} />
         <Card.Body>
-          <Card.Title> {data.country}</Card.Title>
-            <Card.Text>Total Cases: {data.cases}</Card.Text>
-            <Card.Text>Recovered: {data.recovered}</Card.Text>
-            <Card.Text>Deaths: {data.deaths}</Card.Text>
-            <Card.Text>Today's cases: {data.todayCases}</Card.Text>
-            <Card.Text>Today's deaths: {data.todayDeaths}</Card.Text>
-            <Card.Text>Active Cases: {data.active}</Card.Text>
+          <Card.Title  > {data.country}</Card.Title>
+            <Card.Text>Total Cases: {data.cases.toLocaleString()}</Card.Text>
+            <Card.Text>Recovered: {data.recovered.toLocaleString()}</Card.Text>
+            <Card.Text>Deaths: {data.deaths.toLocaleString()}</Card.Text>
+            <Card.Text>Today's cases: {data.todayCases.toLocaleString()}</Card.Text>
+            <Card.Text>Today's deaths: {data.todayDeaths.toLocaleString()}</Card.Text>
+            <Card.Text>Active Cases: {data.active.toLocaleString()}</Card.Text>
 
            
          
@@ -79,17 +106,21 @@ const [searchDatar, setSearchDatar]=useState("")
     )
   })
   
+ 
+  
     return (
       <div className="Checker">
+         {loading ? 
+       ( <div >
        <h2>Covid-19 Statistics</h2>
        <hr className="covidcheckerhorizontal"></hr>
       <CardGroup>
-  <Card className="mb-2" bg="secondary" style={{ margin: '10px' }} text="white">
+  <Card className="mb-2" bg="secondary" style={{ margin: '10px', }} text="white">
     
     <Card.Body>
       <Card.Title>Total Cases</Card.Title>
       <Card.Text>
-        {latest.cases}
+        {latest.cases.toLocaleString()}
       </Card.Text>
     </Card.Body>
     <Card.Footer>
@@ -101,19 +132,19 @@ const [searchDatar, setSearchDatar]=useState("")
     <Card.Body>
       <Card.Title>Deaths</Card.Title>
       <Card.Text>
-      {latest.deaths}
+     {latest.deaths.toLocaleString()} 
       </Card.Text>
     </Card.Body>
     <Card.Footer>
       <small>Last updated {latestUpdated}</small>
     </Card.Footer>
   </Card>
-  <Card className="mb-2" bg="secondary" style={{ margin: '10px' }} text="white">
+  <Card className="mb-2" bg="success" style={{ margin: '10px' }} text="white">
     
     <Card.Body>
       <Card.Title>Recovered</Card.Title>
       <Card.Text>
-     {latest.recovered}
+     {latest.recovered.toLocaleString()}
       </Card.Text>
     </Card.Body>
     <Card.Footer>
@@ -121,22 +152,29 @@ const [searchDatar, setSearchDatar]=useState("")
     </Card.Footer>
   </Card>
 </CardGroup>
+
 <Form>
   <Form.Group className="mb-3" controlId="formGroupSearch" style={{
     display:"flex"
   }}>
     
-    <Form.Control type="text" placeholder="Enter Country name" value={country} onChange={e => setCountry(e.target.value)} />
+    <Form.Control type="text" placeholder="Enter Country name" value={country} onChange={e => setCountry(e.target.value)}
+    style={{marginTop:"30px",position:"relative",border:"3px solid black",marginBottom:"25px",marginLeft:"20px",marginRight:"20px"}} />
   <div className="resetButton" onClick={e => reset()}>Reset</div>
   </Form.Group>
 
   
 </Form>
 
-<Row xs={1} md={3} className="g-4">{countryCases}
+<Row xs={1} md={4} className="g-4">{countryCases}
       </Row>
 
+      
+      </div>)
+ 
 
+      : (<Spinner animation="border" id="spin" 
+      />)}
       </div>
     );
   }
